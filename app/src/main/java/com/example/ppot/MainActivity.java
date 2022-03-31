4dd49cb909471c93460423431f08309dcf022314
Carlos Montiel <carlosmontiel0204@gmail.com>
2022-03-31 01:21:15 -0500

    Creacion de Singleton, logica de juego que muestra quien gana e historia y pick de PC, mapa de jugador y pc que cuenta quien gana por medio de Singleton.

--- app/src/main/java/com/example/ppot/MainActivity.java
+++ app/src/main/java/com/example/ppot/MainActivity.java
@@ -3,12 +3,135 @@ package com.example.ppot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton piedrabt;
+    private ImageView imageView;
+    private ImageButton papelbt;
+    private ImageButton tijerabt;
+    private Button histbt;
+    private Button roundbt;
+    private Random rand=new Random();
+    private MatchTracker matchTracker=MatchTracker.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Enlace de componentes
+        piedrabt=(ImageButton) findViewById(R.id.piedrabt);
+        imageView=(ImageView) findViewById(R.id.imageView);
+        papelbt=(ImageButton) findViewById(R.id.papelbt);
+        tijerabt=(ImageButton) findViewById(R.id.tijerabt);
+        histbt=(Button) findViewById(R.id.histbt);
+        roundbt=(Button) findViewById(R.id.roundbt);
+        piedrabt.setOnClickListener(ev1);
+        papelbt.setOnClickListener(ev2);
+        tijerabt.setOnClickListener(ev3);
+        roundbt.setOnClickListener(ev4);
     }
+    private final View.OnClickListener ev1=new View.OnClickListener(){//Evento usando piedra
+
+        @Override
+        public void onClick(View view) {
+        Integer num=rand.nextInt(2);//Del 0 al 2, 0 piedra 1 papel 2 tijera
+        if(num==0)//piedra
+        {
+            //Empate,muestra imagen de roca de PC
+            matchTracker.setRound(0,num);
+            imageView.setImageResource(R.drawable.rock);
+
+        }
+        else if(num==1)//papel
+        {
+            //Perdida, mostrar imagen, gana PC
+            matchTracker.setRound(0,num);
+            imageView.setImageResource(R.drawable.paper);
+        }
+        else//tijera
+        {
+            //Gana, mostrar imagen
+            matchTracker.setRound(0,num);
+            imageView.setImageResource(R.drawable.scissors);
+        }
+        piedrabt.setEnabled(false);
+        tijerabt.setEnabled(false);
+        papelbt.setEnabled(false);
+
+        }
+    };
+    private final View.OnClickListener ev2=new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Integer num=rand.nextInt(2);//Del 0 al 2, 0 piedra 1 papel 2 tijera
+            if(num==0)
+            {//Jugador Gana, mostrar en imagen
+                matchTracker.setRound(1,num);
+                imageView.setImageResource(R.drawable.rock);
+
+            }
+            else if(num==1) {
+                matchTracker.setRound(1,num);//Empate
+                imageView.setImageResource(R.drawable.paper);
+            }
+            else
+            {
+                //Pierde mostrar en imagen
+                matchTracker.setRound(1,num);
+                imageView.setImageResource(R.drawable.scissors);
+            }
+            piedrabt.setEnabled(false);
+            tijerabt.setEnabled(false);
+            papelbt.setEnabled(false);
+
+        }
+    };
+    private final View.OnClickListener ev3=new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Integer num=rand.nextInt(2);//Del 0 al 2, 0 piedra 1 papel 2 tijera
+            if(num==0)
+            {
+                //Perdida
+                matchTracker.setRound(2,num);
+                imageView.setImageResource(R.drawable.rock);
+            }
+            else if(num==1)
+            {
+                //Gana
+                matchTracker.setRound(2,num);
+                imageView.setImageResource(R.drawable.paper);
+            }
+            else
+            {
+                //Empate
+                matchTracker.setRound(2,num);
+                imageView.setImageResource(R.drawable.scissors);
+            }
+            piedrabt.setEnabled(false);
+            tijerabt.setEnabled(false);
+            papelbt.setEnabled(false);
+
+
+        }
+    };
+    private final View.OnClickListener ev4=new View.OnClickListener()
+    {
+
+        @Override
+        public void onClick(View view) {
+            piedrabt.setEnabled(true);
+            tijerabt.setEnabled(true);
+            papelbt.setEnabled(true);
+        }
+    };
 }
---
+++ app/src/main/java/com/example/ppot/MatchTracker.java
@@ -0,0 +1,88 @@
+package com.example.ppot;
+import java.util.*;
+
+public class MatchTracker {
+    private static MatchTracker instancia=new MatchTracker();
+    private List<String> roundList;
+    private Map<String,Integer> winMap;
+    private Integer pcWins;
+    private Integer playerWins;
+    private Integer roundCounter;
+
+    public static MatchTracker getInstance()
+    {
+        return instancia;
+    }
+    private MatchTracker()
+    {
+        roundList=new ArrayList<String>();
+        winMap=new LinkedHashMap<String,Integer>();
+        winMap.put("Jugador",0);
+        winMap.put("PC",0);
+        pcWins=0;
+        playerWins=0;
+        roundCounter=0;
+    }
+    public void reset()
+    {
+        roundList.clear();
+        winMap.clear();
+        winMap.put("Jugador",0);
+        winMap.put("PC",0);
+        roundCounter=0;
+        pcWins=0;
+        playerWins=0;
+    }
+    public void setRound(Integer player, Integer pc)
+    {
+        roundCounter++;
+        if(player==0 && pc==0 )
+        {
+            roundList.add("Jugador empato Ronda: "+roundCounter+" usando ambos piedra.");
+        }
+        else if(player==0 && pc==1)
+        {
+            roundList.add("Jugador perdio Ronda: "+roundCounter+" usando piedra. PC uso papel.");
+            pcWins++;
+            winMap.put("PC",pcWins);
+        }
+        else if(player==0 && pc==2 )
+        {
+            roundList.add("Jugador gano Ronda: "+roundCounter+" usando piedra. PC uso tijera.");
+            playerWins++;
+            winMap.put("Jugador",playerWins);
+        }
+        else if(player==1 && pc==0 )
+        {
+            roundList.add("Jugador gano Ronda: "+roundCounter+" usando papel. PC uso piedra.");
+            playerWins++;
+            winMap.put("Jugador",playerWins);
+        }
+        else if(player==1 && pc==1 )
+        {
+            roundList.add("Jugador empato Ronda: "+roundCounter+" usando ambos papel.");
+        }
+        else if(player==1 && pc==2 )
+        {
+            roundList.add("Jugador perdio Ronda: "+roundCounter+" usando papel. PC uso tijera.");
+            pcWins++;
+            winMap.put("PC",pcWins);
+        }
+        else if(player==2 && pc==0 )
+        {
+            roundList.add("Jugador perdio Ronda: "+roundCounter+" usando tijera. PC uso piedra.");
+            pcWins++;
+            winMap.put("PC",pcWins);
+        }
+        else if(player==2 && pc==1 )
+        {
+            roundList.add("Jugador gano Ronda: "+roundCounter+" usando tijera. PC uso papel.");
+            playerWins++;
+            winMap.put("Jugador",playerWins);
+        }
+        else if(player==2 && pc==2 )
+        {
+            roundList.add("Jugador empato Ronda: "+roundCounter+" usando ambos tijera.");
+        }
+    }
+}
